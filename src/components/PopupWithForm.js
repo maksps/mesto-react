eb3d793a5fd2ddb8f0f8b7e4f0326caaae3bf8b9
Pavel Maksimov <maksps@yandex.ru>
2022-10-23 23:25:18 +0500

    header

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,19 +1,18 @@
 import React from "react";
 import '../index.css';
 
-function PopupWithForm({title, name, children}) {
-    return ( 
-        <div className= {`popup popup_${name}`}>
-        <div className="popup__container popup__container_edit">
+function PopupWithForm({title, name, children, isOpen, onClose}) {
+    return (
+        <div className={isOpen ? `popup popup_${name} popup_opened` : `popup popup_${name}`}>
+        <div className={`popup__container popup__container_${name}`}>
             <h3 className="popup__title">{title}</h3>
             <form className="popup__form" name={name} noValidate autoComplete="off">
              {children}
             </form>
-            <button className="popup__btn-exit" type="button" aria-label="выход"></button>
+            <button className="popup__btn-exit" onClick={onClose} type="button" aria-label="выход"></button>
         </div>
     </div>
     )
-
 }
 
 export default PopupWithForm;
