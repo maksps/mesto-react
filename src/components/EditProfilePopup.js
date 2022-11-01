62653935a42e3b8e4ace4fb02536ce10dc423a85
Pavel Maksimov <maksps@yandex.ru>
2022-11-02 00:09:56 +0500

    refactor: editProfile

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PopupWithForm from "./PopupWithForm";
 
-function EditProfilePopup({isOpen, onClose}) {
+function EditProfilePopup({ isOpen, onClose }) {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    function handleChangeName(e) {
+        setName(e.target.value);
+        console.log(e.target.value);
+    };
+
+    function handleChangeDescription(e) {
+        setDescription(e.target.value);
+        console.log(e.target.value);
+    };
+
+
+
     return (
         <PopupWithForm
             title={'Редактировать профиль'}
@@ -10,10 +25,10 @@ function EditProfilePopup({isOpen, onClose}) {
             onClose={onClose}
             textButton={'Сохранить'}>
             <div className="input">
-                <input type="text" placeholder="Имя" name="nameInput" className="input__text input__text_type_name"
+                <input type="text" value={name} onChange={handleChangeName} placeholder="Имя" name="nameInput" className="input__text input__text_type_name"
                     minLength="2" maxLength="40" required />
                 <span className="popup__input-error nameInput-error"></span>
-                <input type="text" placeholder="О себе" name="jobInput" className="input__text input__text_type_job"
+                <input type="text" value={description} onChange={handleChangeDescription} placeholder="О себе" name="jobInput" className="input__text input__text_type_job"
                     minLength="2" maxLength="200" required />
                 <span className="popup__input-error jobInput-error"></span>
             </div>
